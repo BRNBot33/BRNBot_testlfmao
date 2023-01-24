ebd3f6e2a96ee64949b3d3e51def17395acc78a7
z2442 <z2442@aol.com>
2023-01-23 19:50:15 -0500

    Disable OTRGui for psp

--- scripts/psp/psp.sh
+++ scripts/psp/psp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmake -H. -Bbuild-psp -GNinja -DCMAKE_TOOLCHAIN_FILE=$PSPDEV/psp/share/pspdev.cmake -DCMAKE_BUILD_TYPE:STRING=Release
+cmake -H. -Bbuild-psp -GNinja -DCMAKE_TOOLCHAIN_FILE=$PSPDEV/psp/share/pspdev.cmake -DCMAKE_BUILD_TYPE:STRING=Release 
 cmake --build build-psp --target soh_pbp --config Release
